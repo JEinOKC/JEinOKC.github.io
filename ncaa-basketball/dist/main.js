6c4994408d81072857980801c98351e09b1796c6
James England <james@Jamess-MacBook-Air.local>
2018-03-11 15:01:43 -0500

    new ncaa basketball ratings for 3.11.2018. Created bracket information for 2018, but its currently just the same as 2017

--- ncaa-basketball/dist/main.js
+++ ncaa-basketball/dist/main.js
@@ -456,7 +456,7 @@
 
 		$.when(
 			$.get('https://static.jameseng.land/ncaa-basketball/data/ratings-latest.json'),
-			$.get('https://static.jameseng.land/ncaa-basketball/bracketology/data/in/bracket-information-2017.json')
+			$.get('https://static.jameseng.land/ncaa-basketball/bracketology/data/in/bracket-information-2018.json')
 		).done(function(ratingsResp,bracketResp){
 
 			var bracketJSON = bracketResp[0];
