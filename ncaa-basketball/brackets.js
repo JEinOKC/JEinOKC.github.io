b7d03f5a405561ff276c6a5c07b577ca7ef36e27
James England <james@MacBook-Pro-2.local>
2023-03-12 23:22:26 -0500

    updated ncaa basketball for 2023

--- ncaa-basketball/brackets.js
+++ ncaa-basketball/brackets.js
@@ -7,7 +7,7 @@ var data = JSON.parse(
 	fs.readFileSync(
 		path.join(
 			__dirname,
-			'bracketology/data/in/bracket-information-2018.json'
+			'bracketology/data/in/bracket-information-2023.json'
 		),
 		'UTF-8'
 	) 
